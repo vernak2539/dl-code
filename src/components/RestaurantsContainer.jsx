6417215b0be8161f4332972da70cb25f1d382aef
Alex Vernacchia <avernacchia@salesforce.com>
2018-08-24 12:39:09 +0100

    add deliveroo font, fix restaurant container loading indicator

--- src/components/RestaurantsContainer.jsx
+++ src/components/RestaurantsContainer.jsx
@@ -3,7 +3,13 @@ import Restaurant from "./Restaurant";
 
 export default ({ restaurants }) => {
   if (!restaurants.length) {
-    return <div className="section">Loading...</div>;
+    return (
+      <div className="section">
+        <div className="restaurant-count text-gray hide-when-small">
+          Loading...
+        </div>
+      </div>
+    );
   }
 
   return (
